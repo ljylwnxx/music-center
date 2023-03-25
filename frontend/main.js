7c1f65c725fafa6a7c60f8fed95b10d91b7edd0e
ljylwnxx <ljylwnxx@163.com>
2023-03-25 16:18:39 +0800

    chore: 整合前后端代码

---
+++ frontend/main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadURL('http://localhost:8081')
+  }
+  app.whenReady().then(() => {
+    createWindow()
+  })
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
+  app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+  })
